d7ffe9be31e29dd8faaf2ccdaaac85df74c00079
Elek, Márton <elek@users.noreply.github.com>
2016-03-06 16:28:50 +0100

    fix: register episode in the service registry

--- src/main/java/hu/tilos/radio/backend/EpisodeStarter.java
+++ src/main/java/hu/tilos/radio/backend/EpisodeStarter.java
@@ -3,11 +3,13 @@ package hu.tilos.radio.backend;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cache.annotation.EnableCaching;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.context.annotation.Configuration;
 
 @SpringBootApplication
 @Configuration
 @EnableCaching
+@EnableEurekaClient
 public class EpisodeStarter {
     public static void main(String[] args) {
         SpringApplication.run(EpisodeStarter.class, args);
