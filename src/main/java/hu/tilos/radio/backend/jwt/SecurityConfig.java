681ac480c858e642618d7b9c1fda982ce0d94285
Elek Márton <elek@apache.org>
2021-01-17 12:10:18 +0100

    [fix] permission for auth services

--- src/main/java/hu/tilos/radio/backend/jwt/SecurityConfig.java
+++ src/main/java/hu/tilos/radio/backend/jwt/SecurityConfig.java
@@ -37,6 +37,10 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
         http.csrf().disable();
         http.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS);
         http
+                .authorizeRequests().antMatchers(HttpMethod.POST, "/api/v1/auth/login").permitAll()
+                .and()
+                .authorizeRequests().antMatchers(HttpMethod.POST, "/api/v1/auth/password_reset").permitAll()
+                .and()
                 .authorizeRequests().antMatchers(HttpMethod.POST, "/api/v1/show/*/contact").permitAll()
                 .and()
                 .authorizeRequests().antMatchers(HttpMethod.POST, "/api/**").authenticated()
